9a27b2b0fab01a4eda1cf033615ed0db7e8b5c6a
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-06-14 14:42:16 +0300

    Test on CI via tox

--- tests/test_oblique.py
+++ tests/test_oblique.py
@@ -1,4 +1,4 @@
-from oblique import get_strategies
+from oblique import get_strategies, main
 
 
 def test_all():
@@ -12,3 +12,7 @@ def test_all():
 def test_zero():
     s = get_strategies(editions=set(), extra=False, python=False)
     assert len(s) == 0
+
+
+def test_main():
+    main(edition="1,2,3,4", count=3, extra=False, python=False)
